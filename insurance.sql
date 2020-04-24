dcd006bc1c43de7ec79cf8e48c044dcad3f5ebf2
Sujan Tulachan <tulachan@Sujans-MacBook-Air.local>
2020-04-23 23:44:42 -0400

    missed file

--- insurance.sql
+++ insurance.sql
@@ -1,1 +1,4 @@
-insert into insurance(transaction_id,name_of_insurance
+insert into insurance(transaction_id,name_of_insurance,amount,type_of_insurance) values(null, 'Basic protection', 05.00, 'Reg');
+insert into insurance(transaction_id,name_of_insurance,amount,type_of_insurance) values(null, 'Plus protection', 10.00, 'Plus');
+insert into insurance(transaction_id,name_of_insurance,amount,type_of_insurance) values(null, 'Premium protection', 15.00, 'Prem');
+insert into insurance(transaction_id,name_of_insurance,amount,type_of_insurance) values(null, 'Ultra protection', 20.00, 'Uprm');
